4b689ed43284d3b0cb8f62d3d6745b905d613d83
Minyoung Kang <miinyoung02@gmail.com>
2025-10-19 00:16:09 +0900

    [Bronze I] Title: 평균, Time: 280 ms, Memory: 22920 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        double[] score = new double[N];
+        double max = 0.0;
+
+        for (int i = 0; i < N; i++) {
+            score[i] = sc.nextDouble();
+            if (score[i] > max) max = score[i];
+        }
+
+        for (int i = 0; i < N; i++) score[i] = score[i]/max*100;
+
+        double sum = 0.0;
+        for (int i = 0; i < N; i++) sum += score[i];
+
+        System.out.println(sum/(double)N);
+    }
+}
