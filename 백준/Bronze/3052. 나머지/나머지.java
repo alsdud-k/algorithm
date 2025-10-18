29460b9348c5eb8c8eb1fa9b17b6eef94b0060e2
Minyoung Kang <miinyoung02@gmail.com>
2025-10-19 00:15:30 +0900

    [Bronze II] Title: 나머지, Time: 176 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] remainder = new int[45];
+
+        for (int i = 0; i < 10; i++) {
+            int x = sc.nextInt();
+            remainder[x%42] = 1;
+        }
+
+        int chk = 0;
+        for (int i = 0; i < 42; i++) {
+            if (remainder[i] != 0) chk++;
+        }
+
+        System.out.println(chk);
+    }
+}
