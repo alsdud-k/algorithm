8172a7527f6d341b33a2447eb427281263a8ccbe
Minyoung Kang <miinyoung02@gmail.com>
2025-10-19 00:13:58 +0900

    [Bronze III] Title: 최소, 최대, Time: 1540 ms, Memory: 310804 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int min = 1000000;
+        int max = -1000000;
+
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+
+            if (x < min) min = x;
+            if (x > max) max = x;
+        }
+
+        System.out.printf("%d %d", min, max);
+    }
+}
