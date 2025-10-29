3a637d60b4e4b064462fe9e1933305bfe7ceb727
Minyoung Kang <miinyoung02@gmail.com>
2025-10-29 22:32:06 +0900

    [level 0] Title: 수 조작하기 2, Time: 1133.79 ms, Memory: 381 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int[] numLog) {
+        String answer = "";
+        
+        int before = numLog[0]; 
+        for (int i = 1; i < numLog.length; i++) {
+            if (before + 1 == numLog[i]) answer += 'w';
+            else if (before - 1 == numLog[i]) answer += 's';
+            else if (before + 10 == numLog[i]) answer += 'd';
+            else if (before - 10 == numLog[i]) answer += 'a';
+            before = numLog[i];
+        }
+        
+        return answer;
+    }
+}
